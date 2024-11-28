efda5a2ffd3de41434d19fca54e5f2acd5233beb
sean <sean.hoyal@external.eodc.eu>
2024-11-28 12:50:16 +0100

    accept reformatting

--- stac_rucio/client.py
+++ stac_rucio/client.py
@@ -1,4 +1,4 @@
-from pystac import Asset, Item
+from pystac import Asset, Item, ItemCollection
 from rucio.client import Client
 from rucio.client.downloadclient import DownloadClient
 
@@ -11,28 +11,27 @@ class StacClient:
         self.rucio.whoami()
         self.downloader = DownloadClient()
 
-    def rucio_item(self, item: Item):
+    def rucio_item(self, items: dict):
         """Take a stac item, and extend the assets with the files as available from Rucio."""
 
-        config = RucioStac(**item.assets["mfcover"].extra_fields["rucio:config"])
+        for item in items["features"]:
+            config = RucioStac(**item["assets"]["mfcover"]["rucio:config"])
 
-        replicas = [
-            replica
-            for replica in self.rucio.list_replicas(
-                dids=[{"scope": config.scope, "name": config.name}],
-                schemes=[
-                    config.scheme,
-                ],
-            )
-        ]
-
-        for replica in replicas:
-            for key, value in replica["rses"].items():
-                item.add_asset(
-                    key, Asset(href=value[0], title=f"Rucio Storage Element: {key}")
+            replicas = [
+                replica
+                for replica in self.rucio.list_replicas(
+                    dids=[{"scope": config.scope, "name": config.name}],
+                    schemes=[
+                        config.scheme,
+                    ],
                 )
+            ]
 
-        return item
+            for replica in replicas:
+                for key, value in replica["rses"].items():
+                    item["assets"][key] = Asset(
+                        href=value[0], title=f"Rucio Storage Element: {key}"
+                    ).to_dict()
 
     def download(self, item, rse):
         """Download from a specic RSE."""
