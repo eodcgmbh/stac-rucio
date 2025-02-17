808e22631b1921852fab1308cf9095f1acde237f
sean <sean.hoyal@external.eodc.eu>
2025-02-17 11:28:19 +0100

    handle replication using replication rules in rucio, update target handling

--- stac_rucio/models.py
+++ stac_rucio/models.py
@@ -6,3 +6,5 @@ class RucioStac(BaseModel):
     scope: str = Field(alias="rucio:scope")
     name: str = Field(alias="rucio:name")
     scheme: str = Field(alias="rucio:scheme", default="https")
+    size: int = Field(alias="rucio:size")
+    adler32: str = Field(alias="rucio:adler32")
